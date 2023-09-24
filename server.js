520974ddf39c428df6c233a77692860227af1e34
meethedapara <meethedapara2002@gmail.com>
2023-09-25 04:00:50 +0530

    some changes

--- server.js
+++ server.js
@@ -2,19 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/UserRoutes");
+const PORT = process.env.PORT || 5000
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://0.0.0.0:27017/netflix",{
+mongoose.connect(process.env.MONGODB_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(()=>{
     console.log("DataBase Connected Successfully")
 });
-
 app.use("/api/user", userRoutes)
 
-app.listen(5000, console.log("server started"));
+app.listen(PORT, console.log("server started"));
